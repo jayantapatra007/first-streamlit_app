f1166a521b3d70b22d30be17fc47a882de7daf11
jayantapatra007 <jayantapatra007@gmail.com>
2023-08-06 03:15:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 #New Section to display fruityvice api response
-streamlit.header(‘Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 
